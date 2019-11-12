17c25fca6c85d75906d59bb42be798e4c4c5e479
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-12 18:57:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,8 +48,8 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  index = starting_point 
-  total = 0 
+  index = 0  
+  total = starting_point
   
   while index < source_array.length do 
     p total += source_array[index]
