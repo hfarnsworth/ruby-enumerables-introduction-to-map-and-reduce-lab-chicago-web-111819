dbf433247304a2e49652785174bf77ec69fccbcf
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-12 18:48:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,7 @@ def reduce_to_total(source_array, starting_point = 0)
     
   end
   while index < source_array.length do 
-    total += source_array[(index - starting_point)]
+    total += source_array[index]
     index += 1 
   end
   
