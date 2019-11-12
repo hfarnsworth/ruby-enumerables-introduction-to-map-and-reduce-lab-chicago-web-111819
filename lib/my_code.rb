58d6bbcfcc6c357d00d09a97b93b093032dd82a7
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-12 18:49:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,7 +48,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  index = starting_point 
+  p index = starting_point 
   total = 0 
 
   if starting_point 
