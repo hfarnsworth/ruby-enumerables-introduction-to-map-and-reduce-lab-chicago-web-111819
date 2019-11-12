741a3d8fe8ef5a6b33c4968066258c5772524d58
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-12 18:25:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,31 +12,81 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+  index = 0 
+  newArray = []
+
+  while index < source_array.length do 
+    newArray[index] = source_array[index]
+    index += 1 
+  end
   
-  
+  newArray
 end
 
 def map_to_double(source_array)
+  index = 0 
+  newArray = []
+
+  while index < source_array.length do 
+    newArray[index] = -1 * source_array[index]
+    index += 1 
+  end
   
-  
+  newArray
 end
 
 def map_to_square(square_array)
+  index = 0 
+  newArray = []
+
+  while index < source_array.length do 
+    newArray[index] = -1 * source_array[index]
+    index += 1 
+  end
+  
+  newArray
   
   
 end
 
 def reduce_to_total(source_array)
+  index = 0 
+  newArray = []
+
+  while index < source_array.length do 
+    newArray[index] = -1 * source_array[index]
+    index += 1 
+  end
+  
+  newArray
   
   
 end
 
 def reduce_to_all_true(source_array)
+  index = 0 
+  newArray = []
+
+  while index < source_array.length do 
+    newArray[index] = -1 * source_array[index]
+    index += 1 
+  end
+  
+  newArray
   
   
 end
 
 def reduce_to_any_true(source_array)
+  index = 0 
+  newArray = []
+
+  while index < source_array.length do 
+    newArray[index] = -1 * source_array[index]
+    index += 1 
+  end
+  
+  newArray
   
   
 end
